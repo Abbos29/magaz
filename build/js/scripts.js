78117e9eee5b72c14f9dff874a7bc79e92467971
abbos <abbosvaliev2003@gmail.com>
2022-09-28 14:56:50 +0500

    feat: add product swiper custom arrows

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -80,8 +80,8 @@ const swiper = new Swiper(".mySwiper", {
 const swiper2 = new Swiper(".mySwiper2", {
     spaceBetween: 10,
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev",
     },
     thumbs: {
         swiper: swiper,
