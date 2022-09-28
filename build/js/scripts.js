ed51f205579273d81e77bd89086616ee615580c3
abbos <abbosvaliev2003@gmail.com>
2022-09-28 14:09:04 +0500

    feat: add product section

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -52,20 +52,43 @@ window.addEventListener('scroll', fixedNav)
 
 // Product swiper
 
-// const swiper = new Swiper(".mySwiper", {
-//     spaceBetween: 10,
-//     slidesPerView: 4,
-//     freeMode: true,
-//     watchSlidesProgress: true,
-// });
+const swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
 
-// const swiper2 = new Swiper(".mySwiper2", {
-//     spaceBetween: 10,
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-//     thumbs: {
-//         swiper: swiper,
-//     },
-// });
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 3,
+          spaceBetween: 10
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 4,
+          spaceBetween: 20
+        },
+
+        720: {
+            slidesPerView: 5,
+            spaceBetween: 20
+        },
+    }
+});
+
+const swiper2 = new Swiper(".mySwiper2", {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: swiper,
+    },
+
+    breakpoints: {
+
+    }
+
+});
